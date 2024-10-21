019c0f291f7305a81b5e67f8f0cb8654ee51854f
AnastasiaNikolaenko <167019379+annast0@users.noreply.github.com>
2024-10-21 12:16:42 +0300

    Update MainWindow.xaml.cs

--- PR-22.101Nikolaenko3/MainWindow.xaml.cs
+++ PR-22.101Nikolaenko3/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace PR_22._101Nikolaenko3
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
